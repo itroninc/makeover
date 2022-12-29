4a3183442cc0c78373377894d145178d7d571759
Greg Bruins <greg.bruins@itron.com>
2022-12-28 16:23:30 -0800

    feat: Add component vnodes

---
+++ src/components/vnodes/VNodes.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'MVNodes',
+
+    props: {
+        vnodes: {
+        }
+    },
+
+    setup(props) {
+        return () => props.vnodes
+    }
+};
+</script>
